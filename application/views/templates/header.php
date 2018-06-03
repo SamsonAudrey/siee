44653b4be2223c8bb4389585fd5b514a86b26b71
Unknown <audrey.samson29@gmail.com>
2018-06-03 17:52:36 +0200

    titre appli

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -2,22 +2,15 @@
 <!DOCTYPE html>
 <html>
 <head>
-  <title>Bootstrap Example</title>
+  <title>SIEE</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-
-  
 
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
   
   <base href = "<?php echo base_url(); ?>">
   <link rel="stylesheet" type="text/css" href="assets/css/style.css">
 
-
-   <!-- <link rel="stylesheet" href="bootstrap.css">
-  <link rel="stylesheet" href="bootstrap-theme.css">
-  <link rel="stylesheet" href="bootstrap-theme.min.css">
-  <link rel="stylesheet" href="bootstrap.min.css"> -->
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 </head>
