f878dfe9b1476318380f6389751a42badfc9a99c
Unknown <audrey.samson29@gmail.com>
2018-06-03 22:10:22 +0200

    final

--- application/views/pages/home.php
+++ application/views/pages/home.php
@@ -17,17 +17,17 @@
 </div>
 
 <div id="list-example" class="list-group">
-  <a class="list-group-item list-group-item-action" href="#list-item-1" id="homepage">Electricité générale</a>
-  <a class="list-group-item list-group-item-action" href="#list-item-2" id="homepage">Contrôle d'accès et vigik</a>
-  <a class="list-group-item list-group-item-action" href="#list-item-3" id="homepage">Antennes collectives</a>
-  <a class="list-group-item list-group-item-action" href="#list-item-4" id="homepage">Hall immeuble</a>
-  <a class="list-group-item list-group-item-action" href="#list-item-5" id="homepage">Automatisme et petites serrureries</a>
-  <a class="list-group-item list-group-item-action" href="#list-item-6" id="homepage">Sécurité/Sinistre</a>
+  <a class="list-group-item list-group-item-action" href="#list-item-1" >Electricité générale</a>
+  <a class="list-group-item list-group-item-action" href="#list-item-2" >Contrôle d'accès et vigik</a>
+  <a class="list-group-item list-group-item-action" href="#list-item-3" >Antennes collectives</a>
+  <a class="list-group-item list-group-item-action" href="#list-item-4" ">Hall immeuble</a>
+  <a class="list-group-item list-group-item-action" href="#list-item-5" >Automatisme et petites serrureries</a>
+  <a class="list-group-item list-group-item-action" href="#list-item-6" >Sécurité/Sinistre</a>
 </div>
-<div data-spy="scroll" data-target="#list-example" data-offset="0" class="scrollspy-example" id="homepage">
-  <h4 id="list-item-1">Electricité générale</h4>
+<div data-spy="scroll" data-target="#list-example" data-offset="0" class="scrollspy-example" >
+  <h4 id="list-item-1" class="titrehome">Electricité générale</h4>
   <p>L'entreprise propose des services d'électricité sur courant faible et courant fort.
-  <ul>
+  <ul class="listehome">
   	<li>Minuterie</li>
   	<li>Eclairage</li>
   	<li>VMC</li>
@@ -35,35 +35,35 @@
   	<li>Détecteur de mouvements</li>
   	<li>...</li>
   </ul> </p>
-  <h4 id="list-item-2">Contrôle d'accès et vigik</h4>
-  <p><ul>
+  <h4 id="list-item-2" class="titrehome">Contrôle d'accès et vigik</h4>
+  <p><ul class="" ="listehome">
   	<li>Interphone</li>
   	<li>Clavier à code</li>
   	<li>Lecteur badge</li>
   	<li>...</li>
   </ul></p>
-  <h4 id="list-item-3">Antennes collectives</h4>
-  <p><ul>
+  <h4 id="list-item-3" class="titrehome">Antennes collectives</h4>
+  <p><ul class="listehome">
   	<li>TNT</li>
   	<li>SAT</li>
   	<li>FM</li>
   	<li>...</li>
   </ul></p>
-  <h4 id="list-item-4">Hall imeuble</h4>
-  <p><ul>
+  <h4 id="list-item-4" class="titrehome">Hall imeuble</h4>
+  <p><ul class="listehome">
   	<li>Porte de hall</li>
   	<li>Boîtes aux lettres</li>
   	<li>...</li>
   </ul></p>
-  <h4 id="list-item-5">Automatisme et petites serrureries</h4>
-  <p><ul>
+  <h4 id="list-item-5" class="titrehome">Automatisme et petites serrureries</h4>
+  <p><ul class="listehome">
   	<li>Portail automatique</li>
   	<li>Barrière automatique</li>
   	<li>Electro gâches</li>
   	<li>...</li>
   </ul></p>
-  <h4 id="list-item-6">Sécurité/Sinistre</h4>
-  <p><ul>
+  <h4 id="list-item-6" class="titrehome">Sécurité/Sinistre</h4>
+  <p><ul class="listehome">
   	<li>Mise en sécurité d'installation électrique</li>
   	<li>Mesures d'isolement</li>
   	<li>...</li>
